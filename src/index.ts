ff17686fbcf7b9b288cf361b8df85fdcadaa5c75
iclare <iandillonclare@gmail.com>
2020-03-31 00:03:48 -0400

    Parse each module and its kwargs to common a format

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import path from 'path';
 import fs from 'fs-extra';
 import sade from 'sade';
+// import execa from 'execa';
 
 import bail from './bail';
 
@@ -16,9 +17,26 @@ export async function main() {
     .action(async (filePath: string, repoPath: string) => {
       const options = { encoding: 'utf-8' };
       const fileContent = await fs.readFile(path.resolve(filePath), options);
-      console.log(filePath);
-      console.log(repoPath);
-      console.log(fileContent.toString());
+
+      const sift = (string: string, using: RegExp) => {
+        const sifted = string.match(using!)?.join('') ?? '';
+        const left = string.replace(using, '');
+        return { sifted, left };
+      };
+
+      const fileParse = fileContent
+        .replace(/\s+|#.*/g, ' ')
+        .replace(/\s+=\s+/g, '=')
+        .split(/(?=\[)/g)
+        .map(x => {
+          const { sifted, left } = sift(x, /\[.*\]/g);
+          const submodule = sifted.replace(/'|"/g, '').replace(/\s+/g, '=');
+          const kwargs = left.trim().split(/\s+/g);
+          return [submodule, ...kwargs];
+        });
+      console.log(fileParse);
     })
     .parse(process.argv);
 }
+
+// .map((x, i) => (i === 0 ? x.replace(/\"|\'/g, '').replace(/\s+/g, '=') : x));
